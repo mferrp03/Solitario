ed1e1b9f8aca3a6b16b9a6ee07074dcb2872c184
miguel <miguel@miguel-VirtualBox>
2018-11-14 12:10:52 +0100

    comentarios

--- src/Baraja.java
+++ src/Baraja.java
@@ -4,9 +4,19 @@ public class Baraja {
 	Baraja() {
 		baraja = new Carta[52];
 	}
+	/*
+	 * Este metodo recibe una carta y el indice de la baraja en el que introducirla,
+	 * comprueba si es una carta que exista (numero y palo validos), comprueba que el indice
+	 * sea valido y si la carta ya esta en la baraja, si todo va bien se añade la carta a la 
+	 * baraja y el metodo devuelve true, si no devuelve un false
+	 */
 	boolean addCarta(int i, Carta carta) {
 		boolean add = false;
-		if (carta.existeCarta() && i>=0 && i<52 && !cartaRepetida(carta)) {
+		if (i==0 && carta.existeCarta()) {
+			baraja[i]=carta;
+			add = true;
+		}
+		if (carta.existeCarta() && i>=1 && i<52 && !cartaRepetida(carta)) {
 			baraja[i]=carta;
 			add = true;
 		}
@@ -15,12 +25,20 @@ public class Baraja {
 	Carta getCarta(int i) {
 		return baraja[i];
 	}
+	/*
+	 * Este metodo recibe una carta y comprueba si esta es identica a alguna que ya se encuentre
+	 * dentro de esta baraja comparando su numero y palo con los de todas las cartas que hay
+	 * actualmente en la baraja, si esta repetida devuelve un true y si no devuelve un false
+	 */
 	boolean cartaRepetida(Carta carta){
 		boolean repetida = false;
 		int i = 0;
 		do {
-			if (carta == getCarta(i)){
-				repetida = true;
+			if (carta.getNum() == getCarta(i).getNum()){
+				if(carta.getPalo() == getCarta(i).getPalo()) {
+					repetida = true;
+					
+				}
 			}
 			i++;
 		}while(!repetida && getCarta(i)!=null && i<52);
@@ -29,3 +47,4 @@ public class Baraja {
 	
 	
 }
+
--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,84 @@ public class Main {
 	public Main(int n) {
 		this.numBarajas = n;
 	}
-	public static void addCarta(Baraja baraja,char numero, char palo, int i) {
+	/*
+	 * Este metodo recibe una baraja, una carta con numero y palo y la posicion de esa 
+	 * carta en la baraja, crea una nueva carta a partir del numero y palo recibidos
+	 * y llama al metodo addCarta de la clase baraja, devuelve un true si se añade correctamente
+	 * y un false si no se ha podido añadir
+	 */
+	public static boolean addCarta(Baraja baraja,char numero, char palo, int i) {
+		boolean add = false;
 		Carta carta = new Carta(numero, palo);
-		baraja.addCarta(i,carta);
+		if(baraja.addCarta(i,carta))
+			add = true;
+		return add;
 	}
 	public static void main(String[]args){
+		boolean correcto = true;
 		int lineas = 0;
 		int numBaraja = 0;
 		int numCarta = 0;
 		ArrayList<Baraja> barajas = new ArrayList<Baraja>();
 		Scanner sc = new Scanner(System.in);
 		Baraja baraja = null;
-		//try {	
+		try {	
+			/*
+			 * se inicializa el contador de caracteres, se lee la linea actual y se guarda en la 
+			 * variable linea y si lineas (el numero de lineas pertenecientes a la baraja actual
+			 * que se han leido con anterioridad) es igual a 0 eso significa que esta es la 
+			 * primera linea de una nueva baraja, por lo que se crea
+			 */
 			do {
 				int contador = 0;
 				String linea = sc.nextLine();
 				if(lineas == 0) {
 					baraja = new Baraja();
 				}
+				/*se comprueba que la linea actual contiene los 52 caracteres necesarios
+				para formar media baraja, se va comprobando caracter a caracter y se suma 1
+				al contador de caracteres por cada uno distinto de un espacio en blanco */
 				for(int i = 0; i<linea.length();i++) {
 					if(linea.charAt(i)!=' ') {
 						contador++;
 				 }	
 			}
+				/*se comprueba el numero de caracteres que se han leido, si se ha leido un numero 
+				 * de caracteres distinto a 52 la entrada es incorrecta, ya que esto significa que se
+				 * ha introducido mas o menos caracteres de los que contiene media baraja
+				 */
 			if (contador!=52) {
 				System.out.println("Entrada incorrecta.1");
 			}
 			else {
+				/*
+				 * se recorre la linea que se acaba de leer, el indice i se coloca sobre el numero de
+				 * la carta, se añade a la baraja utilizando el metodo adCarta, al que se le pasa 
+				 * la baraja que estamos rellenando,el caracter actual sobre el que esta i 
+				 * (numero de la carta), el siguiente caracter(palo) y la posicion de la carta
+				 * en la baraja (numCarta) que se aumenta en cada iteracion, si el metodo 
+				 * addCarta devuelve un false el boolean correcto se pone a false, esto
+				 * se usara luego para enviar un mensaje de error, al final de cada iteracion
+				 * se le suma 3 a i (para colocarlo sobre el numero de la siguiente carta, 
+				 * saltandonos el palo de la carta actual y el espacio entre cartas se le suma 1 
+				 * a numCarta para que la siguiente carta se intente guardar en el siguiente espacio
+				 * de la baraja (si se ha podido añadir la anterior carta), al acabar el bucle 
+				 * se le suma 1 a lineas
+				 */
 				for(int i=0; numCarta<26+26*lineas;i+=3) {
-						addCarta(baraja,linea.charAt(i),linea.charAt(i+1),numCarta);
-						numCarta++;					
+						if(!addCarta(baraja,linea.charAt(i),linea.charAt(i+1),numCarta)) {
+							correcto = false;
+						}
+						else
+							numCarta++;					
 				}
 				lineas++;
 			}
+			/*
+			 * si lineas es = a 2 esto significa que se ha leido una baraja completa, por lo que 
+			 * se añade esa baraja a barajas, se le suma 1 al numero de barajas y las variables
+			 * lineas y numcartas se ponen a 0
+			 */
 			if (lineas == 2) {
 				barajas.add(baraja);
 				numBaraja++;
@@ -46,21 +91,47 @@ public class Main {
 			}
 			
 			}while(!sc.hasNext("#"));
-	//	}catch(Exception e) {
-		//	System.out.println("Entrada incorrecta.2");
-		//}
+			//si no se ha podido incluir una carta pro cualquier motivo correcto estara a false
+			//y se emitira un mensaje de error
+			if (!correcto) {
+				System.out.println("Entrada incorrecta.4");
+			}
+			// tambien se emitira un mensaje de error si se ha cerrado la entrada con una baraja
+			//a medias
+			else if (lineas != 0)
+				System.out.println("Entrada incorrecta.3");
+	}catch(Exception e) {
+			System.out.println("Entrada incorrecta.2");
+		}
 		sc.close();
-		if (lineas != 0)
-			System.out.println("Entrada incorrecta.3");
 		
+		/*
+		 * se entra en un bucle en el que se resuelven todas las barajas una a una, guardandolas
+		 * temporalmente en aux e invocando al metodo solucionar sobre ella
+		 */
 		Main main = new Main(numBaraja);
 		for (int i=0;i<barajas.size();i++) {
 			Baraja aux =barajas.get(i);
 			main.solucionar(aux);
 		}
 	}
+	/* 
+	 * Este metodo es el encargado de mover las cartas de una pila a otra cuando un movimiento
+	 * es factible, recibe la posicion de la carta a mover (k), la distancia del salto
+	 * (1 o 3) y el arraylist de pilas
+	 */
 	boolean moverCartas(int k, int distancia,ArrayList<Pila> pilas) {
 		boolean mover = false;
+		/* 
+		 * se comprueba que la carta de la posicion k y la de k-distancia tengan el mismo
+		 * palo o el mismo numero, es decir, que un movimiento a esta posicion sea 
+		 * factible, en el caso de que sea posible realizar el movimiento se añade la 
+		 * carta de la pila k a la pila k-distancia y se elimina la carta de la pila anterior
+		 * para evitar duplicar la carta, si la pila de la que se ha movido la carta
+		 * queda vacia tras eliminar esta carta se elimina la pila vacia,
+		 * este metodo devuelve un true si se ha podido mover la carta o un false si no
+		 * ha sido posible
+		 */
 		if (pilas.get(k-distancia).getCarta().mismoNumero(pilas.get(k).getCarta()) || pilas.get(k-distancia).getCarta().mismoPalo(pilas.get(k).getCarta())){
 
 			pilas.get(k-distancia).addCarta(pilas.get(k).getCarta());
@@ -76,8 +147,12 @@ public class Main {
 	void solucionar(Baraja baraja) {
 		ArrayList<Pila> pilas = new ArrayList<Pila>();
 		int k = 0;
-			int c = 0;
-			
+		int c = 0;
+			/* 
+			 * el primer paso para solucionar una baraja consiste en colocar cada carta en 
+			 * una pila, por lo que se crea una pila nueva y se añade a esta la siguiente carta
+			 * de la baraja mientras queden cartas en esta
+			 */
 			do {
 				Pila aux = new Pila();
 				aux.addCarta(baraja.getCarta(c));
@@ -85,6 +160,16 @@ public class Main {
 				c++;
 			}while(c<52);
 			do {
+				/*
+				 * k es la pila sobre la que nos encontramos, si k es mayor que 2 esto significa
+				 * que existe una pila tres posiciones a la izquierda, ya que los movimientos
+				 * a tres posiciones tienen prioridad sobre los de 1 se intenta mover la carta
+				 * si no se puede se intenta mover a la posicion inmediatamente a su izquierda
+				 * tras realizar un movimiento k se pone a 0 para volver a comprobar movimientos
+				 * desde la primera pila, si no es posible realizar ningun movimiento se
+				 * suma 1 a k y se pasa a comprobar movimientos para esa pila, el bucle se para
+				 * cuando se llega a la ultima pila y no se realiza ningun movimiento en esta
+				 */
 				if (k > 0) {
 					if(k > 2) {
 						if (moverCartas(k,3,pilas)){
@@ -110,6 +195,11 @@ public class Main {
 			if(pilas.get(0).numCartas() == 52)
 				System.out.println("Ha quedado 1 pila: 52");
 			else {		
+				/*
+				 * se imprime el numero de pilas que han quedad al acabar mirando el tamaño
+				 * del arraylist de pilas y se imprime el numero de cartas de cada una
+				 * utilizando el metodo numCartas
+				 */
 				String salida = new String();
 				salida = "Han quedado "+ (pilas.size()) + " pilas:";
 				for(int j = 0; j<pilas.size(); j++) {
